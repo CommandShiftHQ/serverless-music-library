2287802c72193baab8c90b06f5c8cfee2944d6f1
Ian King <ian.antking@gmail.com>
2022-08-10 09:48:48 +0100

    move aws test configuration to setup file

--- services/create-artist/__tests__/utils/setup.js
+++ services/create-artist/__tests__/utils/setup.js
@@ -1,18 +1,8 @@
 const AWS = require('aws-sdk');
-require('dotenv').config();
-
-const { DYNAMO_DB_PORT } = process.env;
+const setupAws = require('./configureAws');
 
 module.exports = async () => {
-  AWS.config.update({
-    endpoint: `http://localhost:${DYNAMO_DB_PORT}`,
-    region: 'eu-west-2',
-    credentials: {
-      accessKeyId: 'accessKeyId',
-      secretAccessKey: 'secretAccessKey'
-    }
-  });
-
+  setupAws();
   const db = new AWS.DynamoDB();
 
   const params = {
