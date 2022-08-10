4ca69cda08728db6716b73947cebdeadbb063b51
Ian King <ian.antking@gmail.com>
2022-08-10 14:29:18 +0100

    empty database after each test

--- services/create-artist/__tests__/integration/createArtist.test.js
+++ services/create-artist/__tests__/integration/createArtist.test.js
@@ -1,6 +1,36 @@
+const { DynamoDB } = require('aws-sdk');
+const configureAws = require('../utils/configureAws');
 const handler = require('../../handler');
 
 describe('createArtist', () => {
+  let db;
+
+  beforeAll(() => {
+    configureAws();
+    db = new DynamoDB();
+  });
+
+  afterEach(async () => {
+    const params = {
+      TableName: process.env.TABLE_NAME
+    };
+
+    const { Items } = await db.scan(params).promise();
+
+    await Promise.all(Items.map(async (item) => {
+      const { partitionKey, sortKey } = item;
+      const deleteParams = {
+        TableName: process.env.TABLE_NAME,
+        Key: {
+          partitionKey,
+          sortKey,
+        },
+      };
+
+      await db.deleteItem(deleteParams).promise();
+    }));
+  });
+
   it('creates a new artist in the database', async () => {
     const data = {
       name: 'name',
@@ -17,5 +47,23 @@ describe('createArtist', () => {
     expect(payload.name).toBe('name');
     expect(payload.genre).toBe('genre');
     expect(payload.id).toBeTruthy();
+
+    const dbKey = `ARTIST#${payload.id}`;
+    const params = {
+      Key: {
+        partitionKey: {
+          S: dbKey
+        },
+        sortKey: {
+          S: dbKey
+        }
+      },
+      TableName: process.env.TABLE_NAME
+    };
+
+    const { Item } = await db.getItem(params).promise();
+
+    expect(Item.name.S).toBe('name');
+    expect(Item.genre.S).toBe('genre');
   });
 });
